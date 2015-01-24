a293b5266870e7af329a616866709c74e865a60f
Nat Williams <nat.williams@gmail.com>
2015-01-24 12:08:09 -0600

    slightly worse somehow, but cleaned up requests

--- elevator.js
+++ elevator.js
@@ -1,50 +1,53 @@
 {
-    init: function(elevators, floors) {
-        var requestQueue = [];
-        _.each(elevators, function(elevator) {
-
-            elevator.on("idle", function() {
-                var req = requestQueue.shift();
-                console.log(req);
-                if (req === undefined) {
-                    elevator.stop();
-                } else {
-                    elevator.goToFloor(req.floor);
-                }
-            });
-            
-            elevator.on("floor_button_pressed", function(floorNum) {
-                elevator.goToFloor(floorNum);
-            });
-            
-            elevator.on("passing_floor", function(floorNum, direction) {
-                if (elevator.loadFactor() > 0.8) { return; }
-                var floor = floors[floorNum];
-                if (floor.requests[direction]) {
-                    elevator.goToFloor(floorNum, true);
-                    requestQueue = _.reject(requestQueue, function(req) {
-                        return req.floor == floorNum && req.direction == direction
-                    });
-                    floor.requests[direction] = false;
-                }
-            });
-            elevator.on("stopped_at_floor", function(floorNum) { });
-        });
-        
-        _.each(floors, function(floor) {
-            floor.requests = {}
-            floor.on("up_button_pressed", function() {
-                floor.requests.up = true;
-                requestQueue.push({floor: floor.floorNum(), direction: "up"});
-            });
-            
-            floor.on("down_button_pressed", function() {
-                floor.requests.down = true;
-                requestQueue.push({floor: floor.floorNum(), direction: "down"});
-            });
-        });
-    },
-    update: function(dt, elevators, floors) {
-        // We normally don't need to do anything here
+  init: function(elevators, floors) {
+    var requestQueue = {
+      up: [],
+      down: [],
     }
+
+    _.each(elevators, function(elevator) {
+
+      elevator.on("idle", function() {
+        console.log("start idle");
+        var firstUp = requestQueue.up.shift();
+        if (firstUp === undefined) {
+          var firstDown = requestQueue.down.shift();
+          if (firstDown === undefined) {
+            elevator.stop();
+          } else {
+            elevator.goToFloor(firstDown);
+          }
+        } else {
+          elevator.goToFloor(firstUp);
+        }
+        console.log("end idle");
+      });
+
+      elevator.on("floor_button_pressed", function(floorNum) {
+        elevator.goToFloor(floorNum);
+      });
+
+      elevator.on("passing_floor", function(floorNum, direction) {
+        if (elevator.loadFactor() > 0.8) { return; }
+        if (_.contains(requestQueue[direction], floorNum)) {
+          elevator.goToFloor(floorNum, true);
+          requestQueue[direction] = _.without(requestQueue[direction], floorNum)
+        }
+      });
+      elevator.on("stopped_at_floor", function(floorNum) { });
+    });
+
+    _.each(floors, function(floor) {
+      floor.on("up_button_pressed", function() {
+        requestQueue["up"].push(floor.floorNum());
+      });
+
+      floor.on("down_button_pressed", function() {
+        requestQueue["down"].push(floor.floorNum());
+      });
+    });
+  },
+  update: function(dt, elevators, floors) {
+    // We normally don't need to do anything here
+  }
 }
